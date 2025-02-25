10229cc083ad6cb93d40a53d98ad66cefaa4d4c5
Титовец Анастасия <nsttitovec1@gmail.com>
2025-02-25 13:39:04 +0900

    выполненные задания

--- src/main/java/ru/bgpu/annotationlk/AppConfigWorker.java
+++ src/main/java/ru/bgpu/annotationlk/AppConfigWorker.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 public class AppConfigWorker {
 
@@ -17,7 +18,7 @@ public class AppConfigWorker {
 
     public static void configProcessing(String prefix, String filePropName) {
 
-        Reflections reflections = new Reflections(prefix,new FieldAnnotationsScanner());
+        Reflections reflections = new Reflections(prefix, Scanners.FieldAnnotated());
 
         File prop = new File(filePropName);
         if(prop.isFile()) {
@@ -38,7 +39,42 @@ public class AppConfigWorker {
                                 targetValue = value;
                             } else if(field.getType().equals(Integer.class)) {
                                 targetValue = Integer.valueOf(value);
+                            }else if (field.getType().equals(Float.class) || field.getType().equals(float.class)) {
+                                targetValue = Float.valueOf(value);
+                            } else if (field.getType().equals(Double.class) || field.getType().equals(double.class)) {
+                                targetValue = Double.valueOf(value);
+                            } else if (field.getType().isArray()) { // Проверка на массив
+                                Class<?> componentType = field.getType().getComponentType();
+                                if (componentType.equals(String.class)) {
+                                    targetValue = Stream.of(value.split(",")).toArray(String[]::new);
+                                } else if (componentType.equals(Integer.TYPE)) {
+                                    targetValue = Stream.of(value.split(","))
+                                            .mapToInt(Integer::parseInt)
+                                            .toArray();
+                                } else if (componentType.equals(Float.TYPE)) {
+                                    targetValue = Stream.of(value.split(","))
+                                            .map(Float::parseFloat)
+                                            .toArray(Float[]::new);
+                                } else if (componentType.equals(Double.TYPE)) {
+                                    targetValue = Stream.of(value.split(","))
+                                            .mapToDouble(Double::parseDouble)
+                                            .toArray();
+                                } else if (componentType.equals(Integer.class)) {
+                                    targetValue = Stream.of(value.split(","))
+                                            .map(Integer::valueOf)
+                                            .toArray(Integer[]::new);
+                                } else if (componentType.equals(Float.class)) {
+                                    targetValue = Stream.of(value.split(","))
+                                            .map(Float::valueOf)
+                                            .toArray(Float[]::new);
+                                } else if (componentType.equals(Double.class)) {
+                                    targetValue = Stream.of(value.split(","))
+                                            .map(Double::valueOf)
+                                            .toArray(Double[]::new);
+                                }
                             }
+
+
 
                             try {
                                 field.setAccessible(true);
